445c06b076e4a79405a335a7c38a3c641f996dfd
Elian <elifbaque@gmail.com>
2021-11-15 20:30:09 -0300

    Extra

--- src/utils.py
+++ src/utils.py
@@ -5,5 +5,8 @@ def changeFont(string):
     swap = string.swapcase()
     return swap
 
-
 print(changeFont(string))
+
+""" Extra: a- Escribir una funcion que recibe un numero entero
+y imprima por salida estandar(usando print) un triangulo de numeros 
+de altura igual al numero ingresado. Ej. Si se ingresa el numero 5, debe imprimir: 1 22 333 4444 55555 """
