9c096741c061b9f16d6c305af665793a82366001
Soledad Zambrano <soe_28_16@hotmail.com>
2021-11-15 20:49:58 -0300

    ejercicio4

--- src/utils.py
+++ src/utils.py
@@ -10,6 +10,8 @@ print(changeFont(string))
 """ Extra: a- Escribir una funcion que recibe un numero entero
 y imprima por salida estandar(usando print) un triangulo de numeros 
 de altura igual al numero ingresado. Ej. Si se ingresa el numero 5, debe imprimir: 1 22 333 4444 55555 """
+
+#funcion que devuelve los espacios en blanc como guiones ejercicio1
 cadena="soy una cadena"
 
 def replaceString(cadena):
@@ -17,3 +19,12 @@ def replaceString(cadena):
    return cadena
 
 replaceString(cadena)
+
+#funcion que devuelve el nombre y apellido capitalizado ejercicio4
+nomYape="nombre apellido"
+
+def replaceNameAndSurname(cadena):
+    return cadena.title()
+    
+
+print(replaceNameAndSurname(nomYape))
