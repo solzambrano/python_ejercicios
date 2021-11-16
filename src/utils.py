ebf83e7077a00f31ae3ad8d179e101138801a911
Elian <elifbaque@gmail.com>
2021-11-16 05:18:13 -0300

    Ejercicio 3

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,13 @@
+# Ejercicio 1
+
+cadena="soy una cadena"
+
+def replaceString(cadena):
+   cadena= cadena.replace(" ","-")
+   return cadena
+
+replaceString(cadena)
+
 # Ejercicio 2
 
 string = "Cambiando to upper case o LOWER CASE"
@@ -9,22 +19,23 @@ def changeFont(string):
 
 print(changeFont(string))
 
-""" Extra: a- Escribir una funcion que recibe un numero entero
-y imprima por salida estandar(usando print) un triangulo de numeros 
-de altura igual al numero ingresado. Ej. Si se ingresa el numero 5, debe imprimir: 1 22 333 4444 55555 """
+# Ejercicio 3
+oracion = "Las vacas locas son rojas"
+letra = "Z"
+num = 8
+def inmutable(texto, posicion, letra):
+    """Cambia un letra en X posicion del String"""
 
-# Ejercicio 1
+    ListString = list(texto)
+    ListString[posicion] = letra
+    ListString = "".join(ListString)
 
-cadena="soy una cadena"
+    return  print(ListString)
 
-def replaceString(cadena):
-   cadena= cadena.replace(" ","-")
-   return cadena
-
-replaceString(cadena)
-
+inmutable(oracion, num, letra)
 
 # Ejercicio 4
+
 nomYape="nombre apellido"
 
 def replaceNameAndSurname(cadena):
@@ -48,6 +59,7 @@ def segundoPuesto(lista):
 print(segundoPuesto(resultados))
 
 print(replaceNameAndSurname(nomYape))
+
 # Ejercicio Extra 
 
 def triangulo(num):
